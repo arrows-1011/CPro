31cf3f2377a4059bcfc967f6a9cd7bbd859cdfda
arrows-1011 <s1210207@gmail.com>
2015-11-20 01:52:24 +0900

    clean up

--- AOJ/0057.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main(){
-    int n;
-    while(~scanf("%d",&n)){
-	printf("%d\n",(n*n+n+2)/2);
-    }
-    return 0;
-}
--- AOJ/0148.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
- 
-int main(){
-    int N;
-    while(~scanf("%d" ,&N)){
-	printf("3C%02d\n" ,N % 39 ? N % 39 : 39);
-    }
-    return 0;
-}
