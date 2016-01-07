e1e0f274710b39c6f41804a83d0f480646700e5d
arrows-1011 <s1210207@gmail.com>
2016-01-08 00:53:24 +0900

    update

--- AOJ/Volume10/1002.cpp
+++ AOJ/Volume10/1002.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+ 
 using namespace std;
-
+ 
 #define MAX 10001
 #define INF (1<<29)
-
+ 
 int main()
 {
     int Tc,N;
@@ -23,90 +23,63 @@ int main()
             if(i != 0 && s[2*i-1+2*N] == 'Y') b[1][i] = 1;
             if(i != N && s[2*i+2*N] == 'Y') b[1][i] = 1;
         }
-        
+         
         auto solve = [=]()
             {
-                vector<vector<int>> dp(3, vector<int>(MAX, INF));
-                dp[0][0] = 0; dp[1][0] = 1; dp[2][0] = 2;
-		if(b[0][0] == 1 && b[1][0] == 1)
-		{
-		    dp[0][0] = 3;
-		    dp[1][0] = 2;
-		    dp[2][0] = 2;
-		}
-		if(b[0][0] == 1 && b[1][0] == 0)
-		{
-		    dp[0][0] = 1;
-		    dp[1][0] = 1;
-		    dp[2][0] = 2;
-		}
-		if(b[0][0] == 0 && b[1][0] == 1)
-		{
-		    dp[0][0] = 3;
-		    dp[1][0] = 2;
-		    dp[2][0] = 2;
-		}
-		if(b[0][0] == 0 && b[1][0] == 0)
-		{
-		    dp[0][0] = 0;
-		    dp[1][0] = 1;
-		    dp[2][0] = 2;
-		}
-		    
+                vector<vector<int>> dp(2, vector<int>(MAX, INF));
+                if(b[0][0] == 1 && b[1][0] == 1)
+                {
+                    dp[1][0] = 2;
+                }
+                if(b[0][0] == 1 && b[1][0] == 0)
+                {
+                    dp[0][0] = 1;
+                    dp[1][0] = 1;
+                }
+                if(b[0][0] == 0 && b[1][0] == 1)
+                {
+                    dp[1][0] = 2;
+                }
+                if(b[0][0] == 0 && b[1][0] == 0)
+                {
+                    dp[0][0] = 0;
+                    dp[1][0] = 1;
+                }
+		
                 for(int i = 1 ; i <= N ; i++)
                 {
                     if(b[0][i] == 1 && b[1][i] == 1)
                     {
                         dp[0][i] = min(dp[0][i],dp[0][i-1]+4);
                         dp[0][i] = min(dp[0][i],dp[1][i-1]+3);
-                        dp[0][i] = min(dp[0][i],dp[2][i-1]+3);
                         dp[1][i] = min(dp[1][i],dp[0][i-1]+3);
                         dp[1][i] = min(dp[1][i],dp[1][i-1]+3);
-                        dp[1][i] = min(dp[1][i],dp[2][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[0][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[1][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[2][i-1]+4);
                     }
                     if(b[0][i] == 1 && b[1][i] == 0)
                     {
                         dp[0][i] = min(dp[0][i],dp[0][i-1]+2);
                         dp[0][i] = min(dp[0][i],dp[1][i-1]+2);
-                        dp[0][i] = min(dp[0][i],dp[2][i-1]+3);
                         dp[1][i] = min(dp[1][i],dp[0][i-1]+2);
                         dp[1][i] = min(dp[1][i],dp[1][i-1]+2);
-                        dp[1][i] = min(dp[1][i],dp[2][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[0][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[1][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[2][i-1]+4);
                     }
                     if(b[0][i] == 0 && b[1][i] == 1)
                     {
                         dp[0][i] = min(dp[0][i],dp[0][i-1]+4);
                         dp[0][i] = min(dp[0][i],dp[1][i-1]+3);
-                        dp[0][i] = min(dp[0][i],dp[2][i-1]+3);
                         dp[1][i] = min(dp[1][i],dp[0][i-1]+3);
                         dp[1][i] = min(dp[1][i],dp[1][i-1]+2);
-                        dp[1][i] = min(dp[1][i],dp[2][i-1]+2);
-                        dp[2][i] = min(dp[2][i],dp[0][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[1][i-1]+2);
-                        dp[2][i] = min(dp[2][i],dp[2][i-1]+2);
                     }
                     if(b[0][i] == 0 && b[1][i] == 0)
                     {
                         dp[0][i] = min(dp[0][i],dp[0][i-1]+1);
                         dp[0][i] = min(dp[0][i],dp[1][i-1]+2);
-                        dp[0][i] = min(dp[0][i],dp[2][i-1]+3);
                         dp[1][i] = min(dp[1][i],dp[0][i-1]+2);
                         dp[1][i] = min(dp[1][i],dp[1][i-1]+1);
-                        dp[1][i] = min(dp[1][i],dp[2][i-1]+2);
-                        dp[2][i] = min(dp[2][i],dp[0][i-1]+3);
-                        dp[2][i] = min(dp[2][i],dp[1][i-1]+2);
-                        dp[2][i] = min(dp[2][i],dp[2][i-1]+1);
                     }
                 }
                 return dp[0][N];
             };
-        
+         
         cout << solve() << endl;
     }   
     return 0;
