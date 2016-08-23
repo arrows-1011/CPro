126a992e78fc48c12082b2421d60751112491c64
arrows-1011 <s1210207@gmail.com>
2016-08-23 15:26:39 +0900

    Fix

--- AOJ/Volume12/1232.cpp
+++ AOJ/Volume12/1232.cpp
@@ -8,39 +8,41 @@ using namespace std;
  
 vector<int> vec;
  
-void make(){
+void make()
+{
     bool prime[MAX];
-    fill(prime,prime+MAX,true);
+    fill(prime, prime+MAX, true);
     prime[0] = prime[1] = false;
-    for(int i = 2 ; i < MAX ; i++){
-	if(prime[i]){
+    for (int i = 2; i < MAX; i++) {
+	if (prime[i]) {
 	    vec.push_back(i);
-	    for(int j = 2*i ; j < MAX ; j+=i){
-		prime[j] = false;
+	    for (int j = 2*i; j < MAX; j+=i) {
+		    prime[j] = false;
 	    }
 	}
     }
 }
  
  
-int main(){
-    int M,A,B;
+int main()
+{
+    int M, A, B;
   
     make();
-    while(cin >> M >> A >> B, (M | A | B)){
-	int a,b,max = 0;
-	for(int i = 0 ; i < (int)vec.size() ; i++){
-	    for(int j = 0 ; j <= i ; j++){
-		if(vec[i]*vec[j] > M) break;
-		if(A*vec[i] <= vec[j]*B){
-		    if(vec[i]*vec[j] > max){
-			max = vec[i]*vec[j];
-			a = vec[j]; b = vec[i];
-		    }
-		}
-	    }
-	}
-	cout << a << " " << b << endl;
+    while (cin >> M >> A >> B, (M | A | B)) {
+    	int a, b, max = 0;
+    	for (int i = 0; i < (int)vec.size(); i++) {
+    	    for (int j = 0; j <= i; j++) {
+    		    if (vec[i]*vec[j] > M) break;
+    		    if (A*vec[i] <= vec[j]*B) {
+    		        if (vec[i]*vec[j] > max) {
+    			        max = vec[i]*vec[j];
+    			        a = vec[j]; b = vec[i];
+    		        }
+    		    }
+    	    }
+    	}
+     	cout << a << " " << b << endl;
     }
     return 0;
 }
